fbeeb0197393522228df63803ece04a42b6fe6ee
Nguyen Thanh Hau <thanhhau097@gmail.com>
2020-05-03 22:32:03 +1000

    update pipeline

--- pipeline.py
+++ pipeline.py
@@ -17,14 +17,12 @@ def pipeline():
         # Input name "Input 1" is converted to pythonic parameter name "input_1" # note the converted name here
         aws_access_key_id='',
         aws_secret_access_key='',
-        output_path=''
     )
 
     # output_path of load data component must be same as input_path of ocr preprocess component
     ocr_preprocess_component = create_ocr_preprocess_component()
     ocr_preprocess_task = ocr_preprocess_component(
         input_path=load_data_task.outputs['output_path'],
-        output_path=''
     )
 
 
