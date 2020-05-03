4e45dae7c32f63d21ab5f1e246561160dc82122a
Nguyen Thanh Hau <thanhhau097@gmail.com>
2020-05-03 18:58:36 +1000

    update pipeline

--- pipeline.py
+++ pipeline.py
@@ -6,12 +6,24 @@ def create_load_data_component():
     return component
 
 
+def create_ocr_preprocess_component():
+    component = kfp.components.load_component_from_file('./components/ocr/ocrpreprocess/component.yaml')
+    return component
+
+
 def pipeline():
     load_data_component = create_load_data_component()
     load_data_task = load_data_component(
         # Input name "Input 1" is converted to pythonic parameter name "input_1" # note the converted name here
         aws_access_key_id='',
         aws_secret_access_key='',
+        output_path=''
+    )
+
+    # output_path of load data component must be same as input_path of ocr preprocess component
+    ocr_preprocess_component = create_ocr_preprocess_component()
+    ocr_preprocess_task = ocr_preprocess_component(
+        input_path='',
         output_path=''
     )
 
